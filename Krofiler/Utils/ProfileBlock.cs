6ab3c2e7cd0516c39012d0093eb762b635cc56dd
Marius Ungureanu <maungu@microsoft.com>
2019-02-18 23:35:52 +0200

    Add ProfileBlock to stopwatch time some pieces of code

---
+++ Krofiler/Utils/ProfileBlock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Krofiler.Utils
+{
+	public class ProfileBlock : IDisposable
+	{
+		readonly Stopwatch sw = new Stopwatch();
+		readonly string header;
+
+		public ProfileBlock(string header)
+		{
+			this.header = header;
+
+			Console.WriteLine("[START]: {0}", header);
+		}
+
+		public void Dispose()
+		{
+			Console.WriteLine("[END]: {0}ms {1}", sw.ElapsedMilliseconds.ToString(), header);
+		}
+	}
+}
